b53d9209f1705dacc242d46c12f8a0633aa5840a
bastianfbr <badmax202@gmail.com>
2023-03-15 13:25:54 +0100

    ✨ Add beginning of file explorer functions

---
+++ file_explorer.sh
@@ -0,0 +1,27 @@
+# Imports
+source colors.sh
+
+function head {
+    echo "$(color_text 'blanc' 'Répertoire courant : ')$(color_text 'jaune_fonce' $(pwd))"
+}
+
+function fichier_repertoire_courant {
+    
+     # Obtenir le nombre de fichiers dans le répertoire courant
+    num_files=$(ls -1q | wc -l)
+
+    # Obtenir la taille totale des fichiers dans le répertoire courant
+    total_size=$(du -sh | awk '{print $1}')
+
+    # Afficher le nombre de fichiers et la taille totale
+    echo -e "$(color_text 'jaune_fonce' "Nombre de fichiers : $num_files")"
+    echo -e "$(color_text 'jaune_fonce' "Taille totale des fichiers : $total_size")"
+}
+
+function nb_sous_repertoires {
+    # Récupérer le nombre de sous-répertoires dans le répertoire courant
+    nb_subdirs=$(find . -maxdepth 1 -type d | wc -l)
+
+    # Afficher le nombre de sous-répertoires
+    echo "Il y a $nb_subdirs sous-répertoires dans le répertoire courant."
+}
