400089b4fb81c7163e93984ffef8365e88c0e02f
bastianfbr <badmaxyt@gmail.com>
2023-03-19 15:39:02 +0100

    🐛 Fixing H-1 time

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
 function infos_system {
     echo -e "$(color_text 'blanc' 'Date et heure du système : ')"
-    echo -e "$(color_text 'jaune_fonce' "$(date -u '+%d-%m-%y') à $(date -u '+%H:%M:%S')")\n"
+    echo -e "$(color_text 'jaune_fonce' "$(date '+%d-%m-%y') à $(date '+%H:%M:%S')")\n"
 }
