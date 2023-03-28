999b4c95c98c2c38500d2e875bd9ccdeacfde94a
bastianfbr <badmaxyt@gmail.com>
2023-03-28 15:48:15 +0200

    💄 Update UI with pause to continue

--- service_explorer.sh
+++ service_explorer.sh
@@ -1,6 +1,7 @@
 function list_unit_files() {
     result=$(ls /etc/init.d)
     color_text "bleu_clair" "$result"
+    read -p "Appuyer sur une touche pour continuer"
 }
 
 function list_running_units() {
@@ -10,24 +11,28 @@ function list_running_units() {
             color_text "vert_fonce" "$(echo $service_name | cut -d'.' -f1)"
         fi
     done
+    read -p "Appuyer sur une touche pour continuer"
 }
 
 function check_service_status() {
     read -p "Saisir le nom du service : " service_name
     result=$(service "$service_name" status)
     color_text "magenta_clair" "$result"
+    read -p "Appuyer sur une touche pour continuer"
 }
 
 function start_service() {
     read -p "Saisir le nom du service à démarrer : " service_name
     result=$(service "$service_name" start)
     color_text "jaune_fonce" "$result"
+    read -p "Appuyer sur une touche pour continuer"
 }
 
 function stop_service() {
     read -p "Saisir le nom du service à arrêter : " service_name
     result=$(service "$service_name" stop)
     color_text "rouge_fonce" "$result"
+    read -p "Appuyer sur une touche pour continuer"
 }
 
 function restart_service() {
